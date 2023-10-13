af7a0ae4348eca81b049b1777b4c0963fe72a3ae
FlyingASea <SAD_2435219469@outlook.com>
2023-10-13 11:34:59 +0800

    change entity

---
+++ backend/entity/UserEntity.py
@@ -0,0 +1,19 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from main import db
+
+
+class User(db.Model):
+    id = db.Column("id", db.String(80), primary_key=True, unique=True, nullable=False)
+    name = db.Column("name", db.String(255), nullable=False)
+    _password_hash_ = db.Column("password", db.String(255), nullable=False)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def setPassword(self, password):
+        self._password_hash_ = generate_password_hash(password)
+
+    def checkPassword(self, password):
+        return check_password_hash(self._password_hash_, password)
